af2c3d36a471f44222c7915234f76a2512643d2f
sakura <sakura@sakura.com>
2018-12-03 22:24:54 +0800

    1.3 循环队列

--- test/com/sakura/queue/QueueTest.java
+++ test/com/sakura/queue/QueueTest.java
@@ -3,6 +3,8 @@ package com.sakura.queue;
 import com.sakura.queue.impl.ArrayQueueImpl;
 import com.sakura.stack.impl.LoopQueueImpl;
 
+import java.util.Random;
+
 public class QueueTest {
 
     public void testArrayQueue() {
@@ -18,6 +20,9 @@ public class QueueTest {
         }
     }
 
+    /**
+     * 测试队列效率
+     */
     public void testLookQueue() {
         Queue<Integer> queue = new LoopQueueImpl<>();
         for (int i = 0; i < 10; i++) {
@@ -31,9 +36,34 @@ public class QueueTest {
         }
     }
 
-    public static void main(String[] args) {
-        new QueueTest().testArrayQueue();
+    private static double testQueue(Queue<Integer> q, int opCount) {
+        long startTime = System.nanoTime();
 
-        new QueueTest().testLookQueue();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+//        new QueueTest().testArrayQueue();
+//        new QueueTest().testLookQueue();
+
+        int opCount = 100000;
+        Queue<Integer> q1 = new ArrayQueueImpl<>();
+        double time1 = testQueue(q1, opCount);
+
+        Queue<Integer> q2 = new LoopQueueImpl<>();
+        double time2 = testQueue(q2, opCount);
+
+        System.out.println(time1);
+        System.out.println(time2);
     }
 }
