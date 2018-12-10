621fbcadcd0b8cca77f77d3521b68c77fe0c7efd
v_wangjiqing <v_wangjiqing@baidu.com>
2018-12-10 19:31:55 +0800

    Set

---
+++ src/com/sakura/conllections/Set.java
@@ -0,0 +1,14 @@
+package com.sakura.conllections;
+
+public interface Set<E> {
+
+    void add(E e);
+
+    void remove(E e);
+
+    boolean contains(E e);
+
+    int getSize();
+
+    boolean isEmpty();
+}
---
+++ src/com/sakura/conllections/impl/BstSet.java
@@ -0,0 +1,31 @@
+package com.sakura.conllections.impl;
+
+import com.sakura.conllections.Set;
+
+public class BstSet<E> implements Set<E> {
+
+    @Override
+    public void add(E e) {
+
+    }
+
+    @Override
+    public void remove(E e) {
+
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
