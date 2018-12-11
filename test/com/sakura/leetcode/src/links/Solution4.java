ff168e043ee0fc8a1a1f3ff6828ce56460680cd8
sakura <sakura@sakura.com>
2018-12-11 20:54:32 +0800

    Set集合

---
+++ test/com/sakura/leetcode/src/links/Solution4.java
@@ -0,0 +1,26 @@
+package com.sakura.leetcode.src.links;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * leetcode 804
+ */
+public class Solution4 {
+
+    public int uniqueMorseRepresentations(String[] words) {
+
+        String[] codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        Set<String> set = new TreeSet<>();
+        for (String word : words) {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                res.append(codes[word.charAt(i) - 'a']);
+            }
+            set.add(res.toString());
+        }
+
+        return set.size();
+    }
+}
